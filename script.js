d54f3f1425d3d5b9a2046813393074f81cd412c0
Anirban Das <anirban140963@gmail.com>
2025-08-18 14:42:50 +0530

    forth commit

--- script.js
+++ script.js
@@ -11,46 +11,71 @@ useLogo.addEventListener("change", ()=> {
     logoUpload.style.display = useLogo.checked ? "block" : "none";
 });
 
+ function showMessage(msg){
+                const container = document.querySelector(".container");
+                let message = document.createElement("div");
+
+                message.className = "error-message";
+                message.innerHTML = msg;
+
+                container.appendChild(message);
+
+                setTimeout(()=> {
+                    message.classList.add("fade-out");
+                    setTimeout(()=> message.remove(), 500)
+                }, 3000)
+            }
+
+
 function generateQr() {
 
             if(qrText.value.length > 0){
         
             if(useLogo.checked && logoUpload.files.length === 0){
-                alert("please upload a logo");
+                showMessage("please upload a logo");
                 return;
             }
 
+           
                 //clear previous QR
             imgBox.classList.add("show-img");
-            imgBox.innerHTML = '<img src="" id="qrImage">';
-            qrImg = document.getElementById("qrImage");
+            imgBox.innerHTML = "";
+            
             
 
                 //create QR with library
             qrContainer = new QRCode(imgBox, {
                 text: qrText.value,
-                correctLevel:QRCode.CorrectLevel.H
+                width: 500,
+                height: 500,
+                correctLevel: QRCode.CorrectLevel.H
             });
 
             //small delay to allow QR to render
-            setTimeout( ()=>{
+            let checkCanvas = setInterval( ()=> {
                 let canvas = imgBox.querySelector("canvas");
                 if(canvas){
+                    clearInterval(checkCanvas);
+
                     if (useLogo.checked && logoUpload.files.length > 0){
                         let ctx = canvas.getContext("2d");
                         let logo = new Image();
                         logo.src = URL.createObjectURL(logoUpload.files[0]);
                         logo.onload = () => {
-                            const logoSize = canvas.width*0.2;
-                            const x = (canvas.width - logoSize) / 2;
-                            const y = (canvas.height - logoSize) / 2;
-                            ctx.drawImage(logo, x , y, logoSize, logoSize);
+                            const maxLogoSize = canvas.width * 0.2;
+                            let ratio = Math.min(maxLogoSize / logo.width,  maxLogoSize / logo.height);
+                            let logoWidth = logo.width * ratio ;
+                            let logoHeight = logo.height * ratio ; 
+                            const x = (canvas.width - logoWidth) / 2;
+                            const y = (canvas.height - logoHeight) / 2;
+                            ctx.drawImage(logo, x , y, logoWidth, logoHeight);
+
                         };
                     }
                     imgBox.classList.add("show-img");
                     document.getElementById("downloadBtn").classList.add("show");
                 }
-            }, 300);
+            }, 50);
           } else{
             qrText.classList.add("error");
             setTimeout(()=>{
@@ -61,10 +86,23 @@ function generateQr() {
 
 function downloadQr(){
     let canvas = imgBox.querySelector("canvas");
-    if(canvas){
+    if(!canvas) return;
+
+        let scale =4;
+        let tempCanvas = document.createElement("canvas");
+        tempCanvas.width = canvas.width * scale;
+        tempCanvas.height = canvas.height * scale;
+        let ctx = tempCanvas.getContext("2d");
+        ctx.scale(scale, scale);
+        ctx.drawImage(canvas, 0 ,0);
+
         let link = document.createElement("a");
-        link.href = canvas.toDataURL("image/png");
+        link.href = tempCanvas.toDataURL("image/png");
         link.download = "qr-code.png";
         link.click();
-    }
+    
+}
+
+function goBack(){
+    location.reload();
 }
